cbfeeeabd8c6a9deacb4896a5fc76b461b583e3a
Brian Johnson <johnson8727@gmail.com>
2022-05-22 23:15:55 -0400

    more updating

--- src/components/layout/nav/main-nav.js
+++ src/components/layout/nav/main-nav.js
@@ -5,22 +5,20 @@ const MainNavigation = styled.nav`
   ul {
     list-style: none;
   }
-
   li {
     display: flex;
   }
-
   &:link {
-    color: var(--clr-primary-text);
+    color: var(--clr-accent-color);
   }
   &:visited {
-    color: var(--clr-primary-text);
+    color: var(--clr-primary-color);
   }
   &:hover {
-    color: var(--clr-primary-text);
+    color: var(--clr-accent-color);
   }
   &:active {
-    color: var(--clr-primary-text);
+    color: var(--clr-primary-color);
     text-decoration: none;
   }
   &:focus {
